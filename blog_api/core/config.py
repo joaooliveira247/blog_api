6511b7b298aeebb353cf61c30ab11bfdd5ee4aa2
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-16 18:59:54 -0300

    :sparkles: add Settings class

---
+++ blog_api/core/config.py
@@ -0,0 +1,24 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Blog API"
+    API_HOST: str = "localhost"
+    API_PORT: int = 5432
+
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: str
+    DB_NAME: str
+
+    @property
+    def postgres_dsn(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", case_sensitive=True
+    )
+
+
+settings = Settings()
