41b8b3cd5150ee7d7a80a5b0ac60ca9d3335decb
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-17 18:26:05 -0300

    :sparkles: add async_session var

--- blog_api/core/database.py
+++ blog_api/core/database.py
@@ -1,7 +1,13 @@
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
+    AsyncSession,
     create_async_engine,
 )
+from sqlalchemy.orm import sessionmaker
 from config import settings
 
 engine: AsyncEngine = create_async_engine(settings.postgres_dsn, expire_on_commit=False)
+
+async_session: AsyncSession = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
