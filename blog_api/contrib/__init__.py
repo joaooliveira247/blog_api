cc110a56739a0d23e4cb3cef67b96bb6a9d4607c
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-18 19:12:27 -0300

    :sparkles: add DatabaseError, UnableCreateEntity in __init__

--- blog_api/contrib/__init__.py
+++ blog_api/contrib/__init__.py
@@ -1,3 +1,4 @@
 from blog_api.contrib.models import BaseModel
+from blog_api.contrib.errors import DatabaseError, UnableCreateEntity
 
-__all__ = ["BaseModel"]
+__all__ = ["BaseModel", "DatabaseError", "UnableCreateEntity"]
