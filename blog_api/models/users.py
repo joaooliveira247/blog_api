30f2715d7fc2379e3a2a2cb4e7c97137a2731fee
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-18 17:47:55 -0300

    :sparkes: add unique constraint in UserModel(username, email)

--- blog_api/models/users.py
+++ blog_api/models/users.py
@@ -4,7 +4,7 @@ from blog_api.contrib import BaseModel
 
 
 class UserModel(BaseModel):
-    username: Mapped[str] = mapped_column(String(255), nullable=False)
-    email: Mapped[str] = mapped_column(TEXT, nullable=False)
+    username: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(TEXT, nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(60), nullable=False)
     role: Mapped[str] = mapped_column(String(30), nullable=False, default="user")
