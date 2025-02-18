c4417f3e35f2b6c748fa667d5aa97891bdc85a5b
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-18 19:01:15 -0300

    :sparkles: add UnableCreateEntity

--- blog_api/contrib/errors.py
+++ blog_api/contrib/errors.py
@@ -5,3 +5,9 @@ class DatabaseError(CustomError):
     def __init__(self):
         self.message: str = "Database integrity error"
         super().__init__()
+
+
+class UnableCreateEntity(CustomError):
+    def __init__(self):
+        self.message: str = "Unable Create Entity: Field value already exists"
+        super().__init__()
