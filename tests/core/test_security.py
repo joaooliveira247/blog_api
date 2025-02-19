edd9b4386d80aeee9062f74150ac7dea2ed43164
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-19 11:38:51 -0300

    :test_tube: add test_check_password_raise_unknow_hash_error

--- tests/core/test_security.py
+++ tests/core/test_security.py
@@ -1,4 +1,6 @@
+from unittest.mock import patch
 from blog_api.core.security import gen_hash, check_password
+from passlib.exc import UnknownHashError
 from pytest import raises
 
 
@@ -21,3 +23,10 @@ def test_check_password_success(password: str):
     hash_passwd = gen_hash(password)
 
     assert check_password(password, hash_passwd)
+
+
+def test_check_password_raise_unknow_hash_error(password: str):
+    with patch(
+        "blog_api.core.security.pwd_context.verify", side_effect=UnknownHashError
+    ):
+        assert check_password(password, "abcd") is False
