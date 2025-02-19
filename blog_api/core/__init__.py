6be29418705318f7413703f62b1aca94c373e0ab
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-19 09:21:18 -0300

    :sparkles: add gen_hash 'n' check_password in __init__

--- blog_api/core/__init__.py
+++ blog_api/core/__init__.py
@@ -1,4 +1,5 @@
 from blog_api.core.config import settings
 from blog_api.core.database import get_session
+from blog_api.core.security import gen_hash, check_password
 
-__all__ = ["settings", "get_session"]
+__all__ = ["settings", "get_session", "gen_hash", "check_password"]
