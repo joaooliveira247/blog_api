9083127c2ac0e56de6bc5fad032ec5d848bb3ecf
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-19 09:00:06 -0300

    :sparkles: add gen_hash func

--- blog_api/core/security.py
+++ blog_api/core/security.py
@@ -1,3 +1,7 @@
 from passlib.context import CryptContext
 
 pwd_context: CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def gen_hash(passwd: str) -> str:
+    return pwd_context.hash(passwd)
