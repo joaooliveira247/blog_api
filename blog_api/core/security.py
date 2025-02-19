015903525829b28badc2dabb09d25ad9af8014ba
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-19 09:05:48 -0300

    :sparkles: add gen_hash verify type

--- blog_api/core/security.py
+++ blog_api/core/security.py
@@ -4,6 +4,8 @@ pwd_context: CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def gen_hash(passwd: str) -> str:
+    if not isinstance(passwd, str):
+        raise ValueError("password must be a string")
     return pwd_context.hash(passwd)
 
 
