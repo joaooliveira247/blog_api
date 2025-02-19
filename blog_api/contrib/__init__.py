8ee3915b66327cb18891c81fd113d0a5adbfb16e
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-19 08:16:47 -0300

    :sparkles: add GenericError in __init__

--- blog_api/contrib/__init__.py
+++ blog_api/contrib/__init__.py
@@ -1,4 +1,4 @@
 from blog_api.contrib.models import BaseModel
-from blog_api.contrib.errors import DatabaseError, UnableCreateEntity
+from blog_api.contrib.errors import DatabaseError, UnableCreateEntity, GenericError
 
-__all__ = ["BaseModel", "DatabaseError", "UnableCreateEntity"]
+__all__ = ["BaseModel", "DatabaseError", "UnableCreateEntity", "GenericError"]
