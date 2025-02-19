2527d284ad9656bcb56758dc70ff171898dd96ee
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-19 09:01:51 -0300

    :sparkles: add check_password func

--- blog_api/core/security.py
+++ blog_api/core/security.py
@@ -5,3 +5,7 @@ pwd_context: CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def gen_hash(passwd: str) -> str:
     return pwd_context.hash(passwd)
+
+
+def check_password(passwd: str, hash_passwd: str) -> bool:
+    return pwd_context.verify(passwd, hash_passwd)
