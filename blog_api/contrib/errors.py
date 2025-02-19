a1240dbceb4f4f9442dc6b8ee6f25d7ccf4f5bcc
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-19 08:15:36 -0300

    :sparkles: add GenericError

--- blog_api/contrib/errors.py
+++ blog_api/contrib/errors.py
@@ -11,3 +11,9 @@ class UnableCreateEntity(CustomError):
     def __init__(self):
         self.message: str = "Unable Create Entity: Field value already exists"
         super().__init__()
+
+
+class GenericError(CustomError):
+    def __init__(self):
+        self.message: str = "Generic Error"
+        super().__init__()
