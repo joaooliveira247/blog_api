e291bdce8e0b5d69b7a19512f164d9e38861dbeb
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-20 18:00:35 -0300

    :test_tube: add test_create_user_raise_database_error

--- tests/repositories/test_users.py
+++ tests/repositories/test_users.py
@@ -2,10 +2,10 @@ from unittest.mock import AsyncMock
 from uuid import UUID
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, OperationalError
 from blog_api.repositories.users import UsersRepository
 from blog_api.models.users import UserModel
-from blog_api.contrib.errors import UnableCreateEntity
+from blog_api.contrib.errors import UnableCreateEntity, DatabaseError
 from pytest import raises
 
 
@@ -43,3 +43,22 @@ async def test_create_user_raise_unable_create_entity_error(
     mock_session.add.assert_called_once_with(mock_user)
     mock_session.commit.assert_called_once()
     mock_session.rollback.assert_called_once()
+
+
+@pytest.mark.asyncio
+async def test_create_user_raise_database_error(
+    mock_session: AsyncSession, mock_user: UserModel
+):
+    repository = UsersRepository(mock_session)
+
+    mock_session.commit = AsyncMock(
+        side_effect=OperationalError("Connection refused", {}, None)
+    )
+    mock_session.rollback = AsyncMock()
+
+    with raises(DatabaseError, match="Database integrity error"):
+        await repository.create(mock_user)
+
+    mock_session.add.assert_called_once_with(mock_user)
+    mock_session.commit.assert_called_once()
+    mock_session.rollback.assert_awaited_once()
