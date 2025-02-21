97449f415f72ef45c86e9d29464cd6c8ae9ff4c1
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-21 09:58:41 -0300

    :wrench: add many_users_data() factories test

--- tests/factories.py
+++ tests/factories.py
@@ -1,4 +1,5 @@
 from faker import Faker
+from blog_api.core.security import gen_hash
 
 fake: Faker = Faker()
 
@@ -8,3 +9,21 @@ def single_user_data() -> dict:
         "username": fake.user_name(),
         "email": fake.email(),
     }
+
+
+def many_users_data() -> list[dict]:
+    hash_pass: list[str] = []
+
+    for _ in range(4):
+        hash_pass.append(gen_hash(fake.password()))
+
+    return [
+        {
+            "id": fake.uuid4(),
+            "username": fake.user_name(),
+            "email": fake.email(),
+            "password": passwd,
+            "role": "user",
+        }
+        for passwd in hash_pass
+    ]
