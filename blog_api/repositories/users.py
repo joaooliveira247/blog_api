e8061d89e0fe333858d5c1255f25eaa4277bb3bf
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-24 17:40:46 -0300

    :sparkles: add update_user method in UsersRepository

--- blog_api/repositories/users.py
+++ blog_api/repositories/users.py
@@ -1,9 +1,15 @@
-import uuid
+from uuid import UUID
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import OperationalError, IntegrityError
 from sqlalchemy import select
 from blog_api.models.users import UserModel
-from blog_api.contrib import DatabaseError, UnableCreateEntity, GenericError
+from blog_api.contrib.errors import (
+    DatabaseError,
+    UnableCreateEntity,
+    GenericError,
+    UnableUpdateEntity,
+    NoResultFound,
+)
 
 
 class UsersRepository:
@@ -39,19 +45,20 @@ class UsersRepository:
             users = result.scalars().all()
             return list(users)
 
-    async def get_user_by_id(self, id: uuid.UUID) -> UserModel | None:
+    async def get_user_by_id(self, id: UUID) -> UserModel | None:
         async with self.db as session:
             try:
                 result = await session.execute(
                     select(UserModel).filter(UserModel.id == id)
                 )
+
+                user = result.scalars().one_or_none()
+                return user
+
             except OperationalError:
                 raise DatabaseError
             except Exception:
                 raise GenericError
-
-            user = result.scalars().one_or_none()
-            return user
 
     async def get_user_by_query(self, query: UserModel) -> UserModel | None:
         async with self.db as session:
@@ -71,3 +78,30 @@ class UsersRepository:
 
             user = result.scalars().one_or_none()
             return user
+
+    async def update_user_password(self, user_id: UUID, new_password: str) -> None:
+        with self.db as session:
+            try:
+                result = await session.execute(
+                    select(UserModel).filter(UserModel.id == user_id)
+                )
+
+                if update_user := result.scalars().one_or_none():
+                    update_user.password = new_password
+
+                    await self.db.flush()
+                    await self.db.commit()
+                    return
+
+                session.rollback()
+                raise NoResultFound
+
+            except OperationalError:
+                await self.db.rollback()
+                raise DatabaseError
+            except IntegrityError:
+                await self.db.rollback()
+                raise UnableUpdateEntity
+            except Exception:
+                await self.db.rollback()
+                raise GenericError
