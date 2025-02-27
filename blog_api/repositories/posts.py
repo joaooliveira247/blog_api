5d4e70113c7838914ca9ebeea54a0dd87589de56
joaooliveira247 <joaooliveira247@gmail.com>
2025-02-27 17:12:29 -0300

    :recycle: refactorPostsRepository Class inheritance BaseRepository

--- blog_api/repositories/posts.py
+++ blog_api/repositories/posts.py
@@ -1,6 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from blog_api.contrib.repositories import BaseRepository
+from blog_api.repositories.users import UsersRepository
 
 
-class PostsRepository:
-    def __init__(self, session: AsyncSession):
-        self.db = session
+class PostsRepository(BaseRepository):
+    def __init__(self, db: AsyncSession, user_repository: UsersRepository):
+        super().__init__(db)
+        self.user_repository = user_repository
