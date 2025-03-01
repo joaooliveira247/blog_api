f09422e75c7675c4fbc4fb74fb79008eb5e19408
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-01 15:58:11 -0300

    :test_tube: add test_create_post_return_no_result_found_in_user_id

--- tests/repositories/test_posts.py
+++ tests/repositories/test_posts.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from blog_api.repositories.posts import PostsRepository
 from blog_api.models.posts import PostModel
 from blog_api.models.posts import UserModel
+from blog_api.contrib.errors import NoResultFound
 from uuid import UUID
 import pytest
 
@@ -27,3 +28,16 @@ async def test_create_post_success(
     mock_session.flush.assert_called_once()
     mock_session.commit.assert_called_once()
     assert mock_post.id == post_id
+
+
+@pytest.mark.asyncio
+async def test_create_post_return_no_result_found_in_user_id(
+    mock_session: AsyncSession, mock_post: PostModel
+):
+    users_repository = AsyncMock()
+    users_repository.get_user_by_id.return_value = None
+
+    posts_repository = PostsRepository(mock_session, users_repository)
+
+    with pytest.raises(NoResultFound, match="Result not found with user_id"):
+        await posts_repository.create_post(mock_post)
