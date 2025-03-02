c4632568e5cbffe7c1c39bf6ee3c210d6da736c1
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-01 21:52:36 -0300

    :sparkles: add PostOut class

--- blog_api/schemas/posts.py
+++ blog_api/schemas/posts.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel, Field
+from blog_api.contrib.schemas import OutMixin
 
 
 class PostBase(BaseModel):
     title: str = Field(..., description="Post title")
     categories: list[str] = Field(..., description="Post categories")
     content: str = Field(..., description="Post content")
+
+
+class PostOut(PostBase, OutMixin):
+    author: str = Field(..., description="Post author username")
