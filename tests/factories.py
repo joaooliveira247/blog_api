69f124fa2a1bc6c6b6fd005c604cb349f41251bf
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-02 17:23:37 -0300

    :wrench: add many_posts_data factorie

--- tests/factories.py
+++ tests/factories.py
@@ -35,3 +35,43 @@ def single_post_data() -> dict:
         "categories": ["fake", "mock", "factory"],
         "content": fake.text(),
     }
+
+
+def many_posts_data() -> list[dict]:
+    return [
+        {
+            "id": fake.uuid4(),
+            "title": "Post 1: Introdução ao Python",
+            "categories": ["Python", "Introdução"],
+            "content": "Este post fala sobre o básico do Python.",
+            "author": fake.user_name(),
+        },
+        {
+            "id": fake.uuid4(),
+            "title": "Post 2: Avançando em Python",
+            "categories": ["Python", "Avançado"],
+            "content": "Este post aborda tópicos mais avançados em Python.",
+            "author": fake.user_name(),
+        },
+        {
+            "id": fake.uuid4(),
+            "title": "Post 3: Dicas de Carreira em Tecnologia",
+            "categories": ["Carreira", "Tecnologia"],
+            "content": "Neste post, damos dicas sobre como crescer na carreira de tecnologia.",
+            "author": fake.user_name(),
+        },
+        {
+            "id": fake.uuid4(),
+            "title": "Post 4: Introdução ao Machine Learning",
+            "categories": ["Machine Learning", "Inteligência Artificial"],
+            "content": "Aqui discutimos o início no mundo do Machine Learning.",
+            "author": fake.user_name(),
+        },
+        {
+            "id": fake.uuid4(),
+            "title": "Post 5: Como Aprender a Programar",
+            "categories": ["Carreira", "Desenvolvimento Pessoal"],
+            "content": "Este post oferece dicas para quem quer começar a programar.",
+            "author": fake.user_name(),
+        },
+    ]
