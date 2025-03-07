40a5e45b8b9a80b6c00b4ead68cc9bdf17f3d572
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-07 20:48:43 -0300

    :test_tube: add test_update_post_raise_database_error

--- tests/repositories/test_posts.py
+++ tests/repositories/test_posts.py
@@ -360,3 +360,20 @@ async def test_update_post_raise_no_result_found(
             await posts_repository.update_post(post_id, mock_update_post)
 
         mock.assert_called_once_with(post_id, mock_update_post)
+
+
+@pytest.mark.asyncio
+async def test_update_post_raise_database_error(
+    mock_session: AsyncMock, post_id: UUID, mock_update_post: dict
+):
+    users_repository = AsyncMock()
+
+    posts_repository = PostsRepository(mock_session, users_repository)
+
+    with patch.object(PostsRepository, "update_post", new_callable=AsyncMock) as mock:
+        mock.side_effect = DatabaseError
+
+        with pytest.raises(DatabaseError, match="Database integrity error"):
+            await posts_repository.update_post(post_id, mock_update_post)
+
+        mock.assert_called_once_with(post_id, mock_update_post)
