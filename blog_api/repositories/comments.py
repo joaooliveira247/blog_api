64e17436afdf3883f0d2d841a7c7d9aa99fe1afd
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-10 18:42:24 -0300

    :sparkles: add user_repository in __init__ CommentsRepository

--- blog_api/repositories/comments.py
+++ blog_api/repositories/comments.py
@@ -1,9 +1,16 @@
 from blog_api.contrib.repositories import BaseRepository
 from blog_api.repositories.posts import PostsRepository
 from sqlalchemy.ext.asyncio import AsyncSession
+from blog_api.repositories.users import UsersRepository
 
 
 class CommentsRepository(BaseRepository):
-    def __init__(self, db: AsyncSession, post_repository: PostsRepository):
+    def __init__(
+        self,
+        db: AsyncSession,
+        post_repository: PostsRepository,
+        user_repository: UsersRepository,
+    ):
         super().__init__(db)
         self.post_repository = post_repository
+        self.user_reposiotry = user_repository
