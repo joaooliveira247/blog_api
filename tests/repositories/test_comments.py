09d2894cb657e763a05db05c2441f4aa95d0b45e
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-11 18:37:04 -0300

    :test_tube: add test_create_comment_raise_unable_create_entity

--- tests/repositories/test_comments.py
+++ tests/repositories/test_comments.py
@@ -2,8 +2,8 @@ import pytest
 from uuid import UUID
 from unittest.mock import AsyncMock, MagicMock
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.exc import OperationalError
-from blog_api.contrib.errors import DatabaseError, NoResultFound
+from sqlalchemy.exc import OperationalError, IntegrityError
+from blog_api.contrib.errors import DatabaseError, NoResultFound, UnableCreateEntity
 from blog_api.repositories.comments import CommentsRepository
 from blog_api.models.comments import CommentModel
 
@@ -92,6 +92,31 @@ async def test_create_comment_raise_database_error(
     )
 
     with pytest.raises(DatabaseError, match="Database integrity error"):
+        await comments_repository.create_comment(mock_comment)
+
+        mock_session.assert_called_once_with(mock_comment)
+        mock_session.rollback.assert_called_once()
+
+
+@pytest.mark.asyncio
+async def test_create_comment_raise_unable_create_entity(
+    mock_session: AsyncSession, mock_comment: CommentModel
+):
+    users_repository = AsyncMock()
+    users_repository.get_user_by_id.return_value = MagicMock()
+
+    posts_repository = AsyncMock()
+    posts_repository.get_post_by_id.return_value = MagicMock()
+
+    mock_session.add.side_effect = IntegrityError("stmt", "params", "orig")
+
+    comments_repository = CommentsRepository(
+        mock_session, posts_repository, users_repository
+    )
+
+    with pytest.raises(
+        UnableCreateEntity, match="Unable Create Entity: Field value already exists"
+    ):
         await comments_repository.create_comment(mock_comment)
 
         mock_session.assert_called_once_with(mock_comment)
