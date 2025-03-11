c6828b418450926bcbd832e8af294db2ffdd675d
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-11 18:18:13 -0300

    :test_tube: add test_create_comment_raise_database_error

--- tests/repositories/test_comments.py
+++ tests/repositories/test_comments.py
@@ -1,8 +1,9 @@
 import pytest
 from uuid import UUID
-from unittest.mock import AsyncMock
+from unittest.mock import AsyncMock, MagicMock
 from sqlalchemy.ext.asyncio import AsyncSession
-from blog_api.contrib.errors import NoResultFound
+from sqlalchemy.exc import OperationalError
+from blog_api.contrib.errors import DatabaseError, NoResultFound
 from blog_api.repositories.comments import CommentsRepository
 from blog_api.models.comments import CommentModel
 
@@ -72,3 +73,26 @@ async def test_create_comment_raise_no_result_found_post_id(
         await comments_repository.create_comment(mock_comment)
 
         mock_session.assert_called_once_with(mock_comment)
+
+
+@pytest.mark.asyncio
+async def test_create_comment_raise_database_error(
+    mock_session: AsyncSession, mock_comment: CommentModel
+):
+    users_repository = AsyncMock()
+    users_repository.get_user_by_id.return_value = MagicMock()
+
+    posts_repository = AsyncMock()
+    posts_repository.get_post_by_id.return_value = MagicMock()
+
+    mock_session.add.side_effect = OperationalError("stmt", "params", "orig")
+
+    comments_repository = CommentsRepository(
+        mock_session, posts_repository, users_repository
+    )
+
+    with pytest.raises(DatabaseError, match="Database integrity error"):
+        await comments_repository.create_comment(mock_comment)
+
+        mock_session.assert_called_once_with(mock_comment)
+        mock_session.rollback.assert_called_once()
