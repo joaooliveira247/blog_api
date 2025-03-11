6159a35ecd335371b53289147e84d3e5b8eafc29
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-11 16:54:40 -0300

    :sparkles: add create_comment in CommentsRepository

--- blog_api/repositories/comments.py
+++ blog_api/repositories/comments.py
@@ -1,7 +1,13 @@
+from blog_api.contrib.errors import NoResultFound
 from blog_api.contrib.repositories import BaseRepository
 from blog_api.repositories.posts import PostsRepository
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import OperationalError, IntegrityError
 from blog_api.repositories.users import UsersRepository
+from blog_api.models.comments import CommentModel
+from blog_api.models.users import UserModel
+from blog_api.models.posts import PostModel
+from blog_api.contrib.errors import DatabaseError, GenericError, UnableCreateEntity
 
 
 class CommentsRepository(BaseRepository):
@@ -14,3 +20,32 @@ class CommentsRepository(BaseRepository):
         super().__init__(db)
         self.post_repository = post_repository
         self.user_reposiotry = user_repository
+
+    async def create_comment(self, comment: CommentModel) -> None:
+        user: UserModel | None = await self.user_reposiotry.get_user_by_id(
+            comment.user_id
+        )
+
+        if not user:
+            raise NoResultFound("user_id")
+
+        post: PostModel | None = await self.post_repository.get_post_by_id(
+            comment.post_id
+        )
+
+        if not post:
+            raise NoResultFound("post_id")
+
+        try:
+            self.db.add(comment)
+            await self.db.flush()
+            await self.db.commit()
+        except OperationalError:
+            await self.db.rollback()
+            raise DatabaseError
+        except IntegrityError:
+            await self.db.rollback()
+            raise UnableCreateEntity
+        except Exception:
+            await self.db.rollback()
+            raise GenericError
