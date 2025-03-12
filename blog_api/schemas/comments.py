4820b5681ecd9becf1b03777dec1d29cb2cfb600
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-12 18:21:40 -0300

    :sparkles: add CommentOut schema

--- blog_api/schemas/comments.py
+++ blog_api/schemas/comments.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel, Field
+from blog_api.contrib.schemas import OutMixin
 
 
 class CommentBase(BaseModel):
     content: str = Field(..., description="Comment content")
+
+
+class CommentOut(CommentBase, OutMixin):
+    post_title: str = Field(..., description="Title post")
+    author: str = Field(..., description="Comment author username")
