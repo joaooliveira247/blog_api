f2ac03ede1ae80a714ddae2f5eb7dd79201162fe
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-14 10:25:39 -0300

    :test_tube: add test_get_comments_by_user_id_raise_generic_error

--- tests/repositories/test_comments.py
+++ tests/repositories/test_comments.py
@@ -417,3 +417,26 @@ async def test_get_comments_by_user_id_raise_database_error(
             await comments_repository.get_comments_by_user_id(user_id)
 
         mock.assert_called_once_with(user_id)
+
+
+@pytest.mark.asyncio
+async def test_get_comments_by_user_id_raise_generic_error(
+    mock_session: AsyncSession, user_id: UUID
+):
+    users_repository = AsyncMock()
+
+    posts_repository = AsyncMock()
+
+    comments_repository = CommentsRepository(
+        mock_session, posts_repository, users_repository
+    )
+
+    with patch.object(
+        CommentsRepository, "get_comments_by_user_id", new_callable=AsyncMock
+    ) as mock:
+        mock.side_effect = GenericError
+
+        with pytest.raises(GenericError, match="Generic Error"):
+            await comments_repository.get_comments_by_user_id(user_id)
+
+        mock.assert_called_once_with(user_id)
