b1c6fab4d5225f46f2e60c206f3bda13afcd17c2
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-14 17:02:45 -0300

    :test_tube: add test_update_comment_raise_database_error

--- tests/repositories/test_comments.py
+++ tests/repositories/test_comments.py
@@ -621,3 +621,26 @@ async def test_update_comment_raise_no_result_found(
             await comments_repository.update_comment(comment_id, mock_comment_update)
 
         mock.assert_called_once_with(comment_id, mock_comment_update)
+
+
+@pytest.mark.asyncio
+async def test_update_comment_raise_database_error(
+    mock_session: AsyncSession, comment_id: UUID, mock_comment_update: str
+):
+    users_repository = AsyncMock()
+
+    posts_repository = AsyncMock()
+
+    comments_repository = CommentsRepository(
+        mock_session, posts_repository, users_repository
+    )
+
+    with patch.object(
+        CommentsRepository, "update_comment", new_callable=AsyncMock
+    ) as mock:
+        mock.side_effect = DatabaseError
+
+        with pytest.raises(DatabaseError, match="Database integrity error"):
+            await comments_repository.update_comment(comment_id, mock_comment_update)
+
+        mock.assert_called_once_with(comment_id, mock_comment_update)
