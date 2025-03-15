ee93bbeb7d82fc48f70d94cedb697737221d1af1
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-15 16:49:53 -0300

    :sparkles: add UserIn schema

--- blog_api/schemas/users.py
+++ blog_api/schemas/users.py
@@ -1,6 +1,21 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field, EmailStr, field_validator
+import re
 
 
 class BaseUser(BaseModel):
     username: str = Field(..., description="Username", min_length=3, max_length=255)
     email: EmailStr = Field(..., description="User Email", min_length=5, max_length=255)
+
+
+class UserIn(BaseUser):
+    password: str = Field(
+        ..., description="User Password", min_length=8, max_length=128
+    )
+
+    @field_validator("password")
+    def validate_password(cls, value: str) -> str:
+        if not re.match(
+            r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$", value
+        ):
+            raise ValueError("Invalid password format")
+        return value
