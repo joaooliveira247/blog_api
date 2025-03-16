3fcd7343da89d0d2c606b585ed89625496c773b2
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-16 15:26:31 -0300

    :test_tube: add test_base_user_schema_username_empty

--- tests/schemas/test_users.py
+++ tests/schemas/test_users.py
@@ -1,6 +1,9 @@
 from typing import Any
+
+import pytest
 from blog_api.schemas.users import BaseUser
 from tests.factories import single_user_data
+from pydantic import ValidationError
 
 
 def test_base_user_schema_success():
@@ -8,3 +11,16 @@ def test_base_user_schema_success():
     schema = BaseUser(**user)
 
     assert schema.model_dump() == user
+
+
+def test_base_user_schema_username_empty():
+    user: dict[str, Any] = single_user_data()
+    user.pop("username")
+
+    with pytest.raises(ValidationError) as err:
+        BaseUser.model_validate(user)
+
+    assert {
+        "type": err.value.errors()[0]["type"],
+        "loc": err.value.errors()[0]["loc"],
+    } == {"type": "missing", "loc": ("username",)}
