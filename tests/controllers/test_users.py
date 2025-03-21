73bd4eb33a20d18bc0ef375285f9e9b23e7677a8
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-21 07:08:51 -0300

    :white_check_mark: add test_create_user_return_201_created

---
+++ tests/controllers/test_users.py
@@ -0,0 +1,30 @@
+from typing import Any
+from unittest.mock import AsyncMock, patch
+from uuid import UUID
+from httpx import AsyncClient
+from fastapi import status
+import pytest
+
+from blog_api.models.users import UserModel
+from blog_api.repositories.users import UsersRepository
+
+
+@pytest.mark.asyncio
+async def test_create_user_return_201_created(
+    client: AsyncClient, mock_user: UserModel, user_id: UUID, users_url: str
+):
+    user_body: dict[str, Any] = {
+        "username": mock_user.username,
+        "email": mock_user.email,
+        "password": "Abc@1234",
+    }
+
+    with patch.object(
+        UsersRepository, "create_user", new_callable=AsyncMock
+    ) as mock_create_user:
+        mock_create_user.return_value = user_id
+
+        response = await client.post(users_url, json=user_body)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.json() == {"id": str(user_id)}
