ff430890bce5db7638a8e0a581052df2057ea700
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-22 21:32:34 -0300

    :sparkles: add redis_dsn property in settingsclass config

--- blog_api/core/config.py
+++ blog_api/core/config.py
@@ -19,6 +19,10 @@ class Settings(BaseSettings):
     def postgres_dsn(self) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
+    @property
+    def redis_dsn(self) -> str:
+        return f"redis://:{self.CACHE_PASSWORD}@{self.CACHE_HOST}:{self.API_PORT}/0"
+
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", case_sensitive=True
     )
