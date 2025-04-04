8bac4993e4ad5e22a5fde5b353d2333aa0aca092
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-03 22:06:58 -0300

    :sparkles: add encode_pydantic_model func to encode model in str to redis

--- blog_api/utils/encoding.py
+++ blog_api/utils/encoding.py
@@ -1,6 +1,9 @@
 from datetime import datetime
+import json
 from typing import Any
 from uuid import UUID
+
+from pydantic import BaseModel
 
 
 def __transform_type_in_str(map: dict[str, Any]) -> dict[str, str]:
@@ -12,3 +15,17 @@ def __transform_type_in_str(map: dict[str, Any]) -> dict[str, str]:
                 map[k] = v.strftime("%d/%m/%Y, %H:%M:%S")
 
     return map
+
+
+def encode_pydantic_model(model: BaseModel | list[BaseModel]) -> str | None:
+    match model:
+        case BaseModel():
+            return json.dumps(__transform_type_in_str(model.model_dump()))
+        case list():
+            models: list[dict[str, Any]] = []
+
+            for m in model:
+                models.append(__transform_type_in_str(m.model_dump()))
+            return json.dumps(models)
+        case _:
+            return None
