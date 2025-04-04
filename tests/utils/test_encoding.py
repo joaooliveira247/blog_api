0a6f709f65bf651419a1b3ab494e352895834a90
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-03 22:11:03 -0300

    :white_check_mark: add test_encode_pydantic_model_success

---
+++ tests/utils/test_encoding.py
@@ -0,0 +1,16 @@
+import json
+from blog_api.utils.encoding import encode_pydantic_model
+
+
+def test_encode_pydantic_model_success(mock_user_out_inserted):
+    encoded = encode_pydantic_model(mock_user_out_inserted)
+    assert_item: dict[str, str] = {
+        "id": str(mock_user_out_inserted.id),
+        "created_at": mock_user_out_inserted.created_at.strftime("%d/%m/%Y, %H:%M:%S"),
+        "updated_at": mock_user_out_inserted.updated_at.strftime("%d/%m/%Y, %H:%M:%S"),
+        "username": mock_user_out_inserted.username,
+        "email": mock_user_out_inserted.email,
+        "role": mock_user_out_inserted.role,
+    }
+
+    assert encoded == json.dumps(assert_item)
