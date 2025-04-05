90c62046c84bf497a36a8e793c50f55b779ac900
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-05 15:55:23 -0300

    :sparkles: add Cache class add method

--- blog_api/core/cache.py
+++ blog_api/core/cache.py
@@ -1,6 +1,15 @@
 from typing import AsyncGenerator
+import pydantic
 from redis.asyncio import ConnectionPool, Redis
+from redis.exceptions import (
+    ConnectionError,
+    TimeoutError,
+    AuthenticationError,
+    DataError,
+)
+from blog_api.contrib.errors import CacheError, EncodingError
 from blog_api.core.config import settings
+from blog_api.utils.encoding import encode_pydantic_model
 
 
 async def get_cache_connection() -> AsyncGenerator[Redis, None]:
@@ -15,3 +24,13 @@ async def get_cache_connection() -> AsyncGenerator[Redis, None]:
 class Cache:
     def __init__(self, cache_conn: Redis):
         self.cache_conn = cache_conn
+
+    async def add(
+        self, key: str, value: pydantic.BaseModel | list[pydantic.BaseModel]
+    ) -> None:
+        try:
+            await self.cache_conn.set(key, encode_pydantic_model(value), ex=360)
+        except (ConnectionError, TimeoutError, AuthenticationError, DataError) as e:
+            raise CacheError(e)
+        except TypeError:
+            raise EncodingError
