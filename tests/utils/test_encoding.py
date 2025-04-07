0d086467a574467baa8af2f77d5c0ec40a4655ea
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-07 20:29:18 -0300

    :white_check_mark: fix test_encode_pydantic_model_success datetime isoformat changes

--- tests/utils/test_encoding.py
+++ tests/utils/test_encoding.py
@@ -8,8 +8,8 @@ def test_encode_pydantic_model_success(mock_user_out_inserted):
     encoded = encode_pydantic_model(mock_user_out_inserted)
     assert_item: dict[str, str] = {
         "id": str(mock_user_out_inserted.id),
-        "created_at": mock_user_out_inserted.created_at.strftime("%d/%m/%Y, %H:%M:%S"),
-        "updated_at": mock_user_out_inserted.updated_at.strftime("%d/%m/%Y, %H:%M:%S"),
+        "created_at": mock_user_out_inserted.created_at.isoformat(),
+        "updated_at": mock_user_out_inserted.updated_at.isoformat(),
         "username": mock_user_out_inserted.username,
         "email": mock_user_out_inserted.email,
         "role": mock_user_out_inserted.role,
