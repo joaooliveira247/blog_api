fd22e5fb212ba108cd90b291d631f3a43cc216cf
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-08 14:29:38 -0300

    :white_check_mark: add test_decode_single_pydantic_model_success

--- tests/utils/test_encoding.py
+++ tests/utils/test_encoding.py
@@ -1,7 +1,8 @@
 import json
 
-from blog_api.utils.encoding import encode_pydantic_model
+from blog_api.utils.encoding import encode_pydantic_model, decode_pydantic_model
 from blog_api.models.users import UserModel
+from blog_api.schemas.users import UserOut
 
 
 def test_encode_pydantic_model_success(mock_user_out_inserted):
@@ -26,3 +27,11 @@ def test_encode_pydandtic_model_return_empty_list():
 def test_encode_pydantic_model_return_none():
     encoded = encode_pydantic_model(UserModel())
     assert encoded is None
+
+
+def test_decode_single_pydantic_model_success(mock_user_out_inserted):
+    encoded_obj = encode_pydantic_model(mock_user_out_inserted)
+
+    decode_obj = decode_pydantic_model(encoded_obj, UserOut)
+
+    assert decode_obj == mock_user_out_inserted
