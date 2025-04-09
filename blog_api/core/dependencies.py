2f917a73efd0fcffac799432f708b7186efaa52c
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-09 18:21:28 -0300

    :sparkles: add CacheDependency

--- blog_api/core/dependencies.py
+++ blog_api/core/dependencies.py
@@ -1,7 +1,11 @@
 from typing import Annotated
 from fastapi import Depends
+from redis import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
 from blog_api.core.database import get_session
+from blog_api.core.cache import get_cache_connection
 
 
 DatabaseDependency: AsyncSession = Annotated[AsyncSession, Depends(get_session)]
+
+CacheDependency: Redis = Annotated[Redis, Depends(get_cache_connection)]
