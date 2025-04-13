cfe92f1450d3e5fb27df211e40fcec1929902675
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-13 16:36:29 -0300

    :alien: fix functions 'cause changes in JWT package

--- blog_api/core/token.py
+++ blog_api/core/token.py
@@ -1,7 +1,7 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import Any
 
-import jwt
+from jose import jwt, ExpiredSignatureError, exceptions
 from blog_api.contrib.errors import GenericError, TokenError
 from blog_api.models.users import UserModel
 from blog_api.core.config import get_settings
@@ -12,8 +12,8 @@ settings = get_settings()
 def gen_jwt(life_time: float, user: UserModel) -> str:
     payload: dict[str, Any] = {
         "sub": str(user.id),
-        "exp": datetime.now() + timedelta(minutes=life_time),
-        "iat": datetime.now(),
+        "exp": datetime.now(timezone.utc) + timedelta(minutes=life_time),
+        "iat": datetime.now(timezone.utc),
         "role": user.role,
     }
 
@@ -24,9 +24,9 @@ def verify_jwt(token: str) -> dict[str, Any] | None:
     try:
         decoded = jwt.decode(token, settings.JWT_SECRET_KEY, settings.JWT_ALGORITHM)
         return decoded
-    except jwt.ExpiredSignatureError as e:
+    except ExpiredSignatureError as e:
         raise TokenError(e)
-    except jwt.InvalidTokenError as e:
+    except exceptions.JWTError as e:
         raise TokenError(e)
     except Exception as e:
         raise GenericError(e)
