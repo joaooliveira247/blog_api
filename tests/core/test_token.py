86af24b8baad4a26f49ace03599edef708395289
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-14 08:17:06 -0300

    :test_tube: add test_verify_jwt_raise_expired_signature_error

--- tests/core/test_token.py
+++ tests/core/test_token.py
@@ -1,3 +1,5 @@
+from pytest import raises
+from blog_api.contrib.errors import TokenError
 from blog_api.core.token import gen_jwt, verify_jwt
 
 
@@ -17,3 +19,10 @@ def test_verify_jwt_success(mock_user_inserted):
     assert isinstance(token_dict, dict)
     assert token_dict["sub"] == str(mock_user_inserted.id)
     assert token_dict["role"] == "user"
+
+
+def test_verify_jwt_raise_expired_signature_error(mock_user_inserted):
+    token = gen_jwt(-90, mock_user_inserted)
+
+    with raises(TokenError, match="Token Error: Signature has expired."):
+        verify_jwt(token)
