702bff3f29a6e6496fb0dfe9c31a270d905579bf
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-16 22:11:33 -0300

    :sparkles: add oauth2_schema var with api path

--- blog_api/dependencies/auth.py
+++ blog_api/dependencies/auth.py
@@ -1,4 +1,7 @@
+from fastapi.security import OAuth2PasswordBearer
 from blog_api.core.config import get_settings
 
 
 settings = get_settings()
+
+oauth2_schema = OAuth2PasswordBearer(tokenUrl=f"{settings.API_PATH}/account/sign-in")
