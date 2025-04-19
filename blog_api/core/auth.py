b991d7915e0d2af7aa04a9970df36d1eef909206
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-19 18:36:54 -0300

    :truck: move authenticate function from dependencies to core

---
+++ blog_api/core/auth.py
@@ -0,0 +1,25 @@
+from pydantic import EmailStr
+from sqlalchemy.ext.asyncio import AsyncSession
+from blog_api.schemas.users import UserOut
+from blog_api.repositories.users import UsersRepository
+from blog_api.models.users import UserModel
+from blog_api.contrib.errors import InvalidResource
+from blog_api.core.security import check_password
+
+
+async def authenticate(
+    db: AsyncSession,
+    email: EmailStr,
+    passwd: str,
+) -> UserOut:
+    user_repository = UsersRepository(db)
+
+    user = await user_repository.get_user_by_query(UserModel(email=email))
+
+    if not user:
+        raise InvalidResource("email")
+
+    if not check_password(passwd, user.password):
+        raise InvalidResource("password")
+
+    return user
--- blog_api/dependencies/auth.py
+++ blog_api/dependencies/auth.py
@@ -1,14 +1,10 @@
 from typing import Any
 from fastapi import HTTPException, status
-from pydantic import EmailStr
-from sqlalchemy.ext.asyncio import AsyncSession
-from blog_api.core.security import check_password
 from blog_api.core.token import verify_jwt
 from blog_api.dependencies.dependencies import DatabaseDependency, TokenDependency
-from blog_api.models.users import UserModel
 from blog_api.repositories.users import UsersRepository
 from blog_api.schemas.users import UserOut
-from blog_api.contrib.errors import InvalidResource, TokenError, GenericError
+from blog_api.contrib.errors import TokenError, GenericError
 
 
 async def get_current_user(
@@ -41,17 +37,3 @@ async def get_current_user(
     except GenericError as e:
         credencial_exception.detail = e.message
         raise credencial_exception
-
-
-async def authenticate(email: EmailStr, passwd: str, db: AsyncSession) -> UserOut:
-    user_repository = UsersRepository(db)
-
-    user = await user_repository.get_user_by_query(UserModel(email=email))
-
-    if not user:
-        raise InvalidResource("email")
-
-    if not check_password(passwd, user.password):
-        raise InvalidResource("password")
-
-    return UserOut(**user.__dict__)
