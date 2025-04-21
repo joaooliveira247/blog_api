a7321a6b5d112b278b9ee677b5b711d268ba5fa8
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-21 08:22:41 -0300

    :sparkles: add login in users_controller with route /account/sign-in

--- blog_api/controllers/users.py
+++ blog_api/controllers/users.py
@@ -1,11 +1,23 @@
-from fastapi import APIRouter, status, Body, HTTPException
+from fastapi import APIRouter, Depends, status, Body, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from blog_api.core.auth import authenticate
+from blog_api.core.config import get_settings
+from blog_api.core.token import gen_jwt
 from blog_api.dependencies.dependencies import DatabaseDependency
 from blog_api.repositories.users import UsersRepository
 from blog_api.models.users import UserModel
 from blog_api.schemas.users import UserIn
-from blog_api.schemas.response import UserCreatedSchema
-from blog_api.contrib.errors import UnableCreateEntity, DatabaseError, GenericError
+from blog_api.schemas.response import TokenResponse, UserCreatedSchema
+from blog_api.contrib.errors import (
+    InvalidResource,
+    TokenError,
+    UnableCreateEntity,
+    DatabaseError,
+    GenericError,
+)
 
+
+settings = get_settings()
 
 users_controller = APIRouter(tags=["account"])
 
@@ -30,3 +42,26 @@ async def create_user(
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=err.message)
     except GenericError:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@users_controller.post("/sign-in", status_code=status.HTTP_200_OK)
+async def login(
+    db: DatabaseDependency,  # type: ignore
+    form_data: OAuth2PasswordRequestForm = Depends(),
+) -> TokenResponse:
+    try:
+        user = await authenticate(db, form_data.username, form_data.password)
+
+        jwt = gen_jwt(settings.JWT_DEFAULT_LIFE_TIME, user)
+
+        return TokenResponse(access_token=jwt)
+    except (DatabaseError, TokenError) as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e.message
+        )
+    except InvalidResource as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e.message)
+    except GenericError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e.message
+        )
