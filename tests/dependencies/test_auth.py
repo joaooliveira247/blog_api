cdef65e4f5ffb9a0f110ad6c0fd625f362d7c270
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-22 17:11:56 -0300

    :test_tube: add test_get_current_user_raise_cache_error

--- tests/dependencies/test_auth.py
+++ tests/dependencies/test_auth.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta, timezone
 from unittest.mock import AsyncMock, patch
 from fastapi import HTTPException
 import pytest
+from blog_api.contrib.errors import CacheError
 from blog_api.core.cache import Cache
 from blog_api.core.token import gen_jwt
 from blog_api.dependencies.auth import get_current_user
@@ -75,6 +76,40 @@ async def test_get_current_user_success_from_cache(
         assert isinstance(result, UserOut)
         assert result.id == mock_user_out_inserted.id
         assert result.email == mock_user_out_inserted.email
+
+        mock_jwt.assert_called_once_with(jwt)
+        mock_user.assert_called_once_with(
+            str(f"user:{mock_user_out_inserted.id}"), UserOut
+        )
+
+
+@pytest.mark.asyncio
+async def test_get_current_user_raise_cache_error(
+    mock_session, cache_session, mock_user_out_inserted
+):
+    user = UserModel(**mock_user_out_inserted.model_dump())
+    jwt = gen_jwt(360, user)
+
+    payload = {
+        "sub": str(user.id),
+        "exp": datetime.now(timezone.utc) + timedelta(minutes=360),
+        "iat": datetime.now(timezone.utc),
+        "role": user.role,
+    }
+
+    with (
+        patch(
+            "blog_api.dependencies.auth.verify_jwt",
+            return_value=payload,
+        ) as mock_jwt,
+        patch.object(
+            Cache,
+            "get",
+            new=AsyncMock(side_effect=CacheError("Cache Error")),
+        ) as mock_user,
+    ):
+        with pytest.raises(HTTPException, match="401: Cache Error"):
+            await get_current_user(mock_session, cache_session, token=jwt)
 
         mock_jwt.assert_called_once_with(jwt)
         mock_user.assert_called_once_with(
