ac6271cd18d9d6b3522d0c88a3a4cf5254121b5c
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-22 16:47:26 -0300

    :sparkles: add cache check in get_current_user

--- blog_api/dependencies/auth.py
+++ blog_api/dependencies/auth.py
@@ -1,14 +1,20 @@
-from typing import Any
+from typing import Any, cast
 from fastapi import HTTPException, status
+from blog_api.core.cache import Cache
 from blog_api.core.token import verify_jwt
-from blog_api.dependencies.dependencies import DatabaseDependency, TokenDependency
+from blog_api.dependencies.dependencies import (
+    CacheDependency,
+    DatabaseDependency,
+    TokenDependency,
+)
 from blog_api.repositories.users import UsersRepository
 from blog_api.schemas.users import UserOut
-from blog_api.contrib.errors import TokenError, GenericError
+from blog_api.contrib.errors import CacheError, EncodingError, TokenError, GenericError
 
 
 async def get_current_user(
     db: DatabaseDependency,  # type: ignore
+    cache: CacheDependency,  # type: ignore
     token: TokenDependency,  # type: ignore
 ) -> UserOut:
     credencial_exception = HTTPException(
@@ -22,6 +28,13 @@ async def get_current_user(
             credencial_exception.detail = "User can't be authenticated"
             raise credencial_exception
 
+        cache_service = Cache(cache_conn=cache)
+
+        user_cache = await cache_service.get(f"user:{user_id}", UserOut)
+
+        if user_cache is not None:
+            return cast(UserOut, user_cache)
+
         user_repository = UsersRepository(db)
         user = await user_repository.get_user_by_id(user_id)
 
@@ -29,9 +42,19 @@ async def get_current_user(
             credencial_exception.detail = "User can't be authenticated"
             raise credencial_exception
 
-        return UserOut(**user.__dict__)
+        user_out = UserOut(**user.__dict__)
+
+        await cache_service.add(f"user:{user_id}", user_out)
+
+        return user_out
 
     except TokenError as e:
+        credencial_exception.detail = e.message
+        raise credencial_exception
+    except CacheError as e:
+        credencial_exception.detail = e.message
+        raise credencial_exception
+    except EncodingError as e:
         credencial_exception.detail = e.message
         raise credencial_exception
     except GenericError as e:
