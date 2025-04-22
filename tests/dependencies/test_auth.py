e4c8b088b4fe23818042eb9cda9060136fb6e8d6
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-22 16:59:27 -0300

    :white_check_mark: add test_get_current_user_success_from_cache

--- tests/dependencies/test_auth.py
+++ tests/dependencies/test_auth.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta, timezone
 from unittest.mock import AsyncMock, patch
 from fastapi import HTTPException
 import pytest
+from blog_api.core.cache import Cache
 from blog_api.core.token import gen_jwt
 from blog_api.dependencies.auth import get_current_user
 from blog_api.models.users import UserModel
@@ -42,6 +43,43 @@ async def test_get_current_user_success(
 
         mock_jwt.assert_called_once_with(jwt)
         mock_user.assert_called_once_with(str(mock_user_out_inserted.id))
+
+
+@pytest.mark.asyncio
+async def test_get_current_user_success_from_cache(
+    mock_session, cache_session, mock_user_out_inserted
+):
+    user = UserModel(**mock_user_out_inserted.model_dump())
+    jwt = gen_jwt(360, user)
+
+    payload = {
+        "sub": str(user.id),
+        "exp": datetime.now(timezone.utc) + timedelta(minutes=360),
+        "iat": datetime.now(timezone.utc),
+        "role": user.role,
+    }
+
+    with (
+        patch(
+            "blog_api.dependencies.auth.verify_jwt",
+            return_value=payload,
+        ) as mock_jwt,
+        patch.object(
+            Cache,
+            "get",
+            new=AsyncMock(return_value=mock_user_out_inserted),
+        ) as mock_user,
+    ):
+        result = await get_current_user(mock_session, cache_session, token=jwt)
+
+        assert isinstance(result, UserOut)
+        assert result.id == mock_user_out_inserted.id
+        assert result.email == mock_user_out_inserted.email
+
+        mock_jwt.assert_called_once_with(jwt)
+        mock_user.assert_called_once_with(
+            str(f"user:{mock_user_out_inserted.id}"), UserOut
+        )
 
 
 @pytest.mark.asyncio
