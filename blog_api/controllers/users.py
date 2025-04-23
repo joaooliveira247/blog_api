3062d55da5f246e826600fb6cf80b4cc8648b3a8
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-23 08:26:11 -0300

    :sparkles: add update_password in users_controller with route /account/password

--- blog_api/controllers/users.py
+++ blog_api/controllers/users.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter, Depends, status, Body, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from blog_api.core.auth import authenticate
 from blog_api.core.config import get_settings
+from blog_api.core.security import check_password
 from blog_api.core.token import gen_jwt
 from blog_api.dependencies.auth import get_current_user
 from blog_api.dependencies.dependencies import DatabaseDependency
 from blog_api.repositories.users import UsersRepository
 from blog_api.models.users import UserModel
-from blog_api.schemas.users import UserIn, UserOut
+from blog_api.schemas.users import PasswordUpdate, UserIn, UserOut
 from blog_api.schemas.response import TokenResponse, UserCreatedSchema
 from blog_api.contrib.errors import (
     InvalidResource,
@@ -15,6 +16,7 @@ from blog_api.contrib.errors import (
     UnableCreateEntity,
     DatabaseError,
     GenericError,
+    UnableUpdateEntity,
 )
 
 
@@ -71,3 +73,39 @@ async def login(
 @users_controller.get("/", status_code=status.HTTP_200_OK)
 async def get_logged_user(user: UserOut = Depends(get_current_user)) -> UserOut:
     return user
+
+
+@users_controller.put("/password", status_code=status.HTTP_204_NO_CONTENT)
+async def update_password(
+    db: DatabaseDependency,  # type: ignore
+    form: PasswordUpdate,  # type: ignore
+    user: UserOut = Depends(get_current_user),
+) -> None:
+    repository: UsersRepository = UsersRepository(db)
+
+    try:
+        user_db = await repository.get_user_by_id(user.id)
+
+        if user_db is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found"
+            )
+
+        if check_password(form.password, user_db.password):
+            raise HTTPException(
+                status_code=status.HTTP_204_NO_CONTENT,
+                detail="New password cannot be the same as current password",
+            )
+
+        await repository.update_user_password(user.id, form.password)
+
+        return None
+
+    except (DatabaseError, UnableUpdateEntity) as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e.message
+        )
+    except GenericError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e.message
+        )
