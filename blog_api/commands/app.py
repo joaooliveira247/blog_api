3656a5de62a68c635af98b283a2616a8e1615e68
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-23 18:16:37 -0300

    :sparkles: add pagination in main application

--- blog_api/commands/app.py
+++ blog_api/commands/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi_pagination import add_pagination
 from blog_api.core.config import get_settings
 from blog_api.urls import api_router
 
 settings = get_settings()
 
 app = FastAPI(title=settings.PROJECT_NAME)
+add_pagination(app)
 app.include_router(api_router)
