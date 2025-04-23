efd98208197fe882447d6fb2b93683a3ac4137ff
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-23 07:55:01 -0300

    :fire: remove password logic moved to PasswordMixin

--- blog_api/schemas/users.py
+++ blog_api/schemas/users.py
@@ -38,33 +38,7 @@ class PasswordMixin(BaseModel):
         return value
 
 
-class UserIn(BaseUser):
-    password: str = Field(
-        ..., description="User Password", min_length=8, max_length=128
-    )
-
-    @field_validator("password")
-    def validate_password(cls, value: str) -> str:
-        pattern = {
-            "lower": r"[a-z]",
-            "upper": r"[A-Z]",
-            "digit": r"\d",
-            "special": r"[@$!%*?&]",
-        }
-
-        components = {
-            key: "".join(re.findall(regex, value)) or None
-            for key, regex in pattern.items()
-        }
-
-        missing_components = [key for key, value in components.items() if value is None]
-
-        if missing_components:
-            raise ValueError(
-                f"Wrong password format! characters missing: {', '.join(missing_components)}"
-            )
-
-        return value
+class UserIn(BaseUser, PasswordMixin): ...
 
 
 class UserOut(BaseUser, OutMixin):
