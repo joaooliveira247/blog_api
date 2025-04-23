69c934ca6feb327e2e8d32ef9335ceeb56feaef9
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-23 18:17:45 -0300

    :sparkles: add admin router

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from blog_api.controllers.users import users_controller
+from blog_api.controllers.admin import admin_controller
 
 api_router = APIRouter()
 api_router.include_router(users_controller, prefix="/account")
+api_router.include_router(admin_controller, prefix="/admin")
