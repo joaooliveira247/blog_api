947481bbb975502094c8e3280af5ed57eba6f679
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-25 13:17:46 -0300

    :test_tube: add test_user_agent_empty_return_400_bad_request

--- tests/middlewares/test_user_agent.py
+++ tests/middlewares/test_user_agent.py
@@ -14,3 +14,14 @@ async def test_user_agent_success(client: AsyncClient):
 
     assert result.status_code == status.HTTP_404_NOT_FOUND
     assert result.json() == {"detail": "Not Found"}
+
+
+@pytest.mark.asyncio
+async def test_user_agent_empty_return_400_bad_request(client: AsyncClient):
+    result = await client.get(
+        "/",
+        headers={"User-Agent": ""},
+    )
+
+    assert result.status_code == status.HTTP_400_BAD_REQUEST
+    assert result.json() == {"detail": "User-Agent empty"}
