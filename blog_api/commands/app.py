17147b76405daa3b58a3dd15bf9bcf5f409d9a8c
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-25 13:19:46 -0300

    :sparkles: registry UserAgentMiddleware in middlewares

--- blog_api/commands/app.py
+++ blog_api/commands/app.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 from blog_api.core.config import get_settings
 from blog_api.urls import api_router
+from blog_api.middlewares.user_agent import UserAgentMiddleware
 
 settings = get_settings()
 
 app = FastAPI(title=settings.PROJECT_NAME)
+app.add_middleware(UserAgentMiddleware)
 app.include_router(api_router)
 add_pagination(app)
