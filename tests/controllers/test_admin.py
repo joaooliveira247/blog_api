6f9dd776d0287a6b458d6e725890c30479bc8814
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-25 17:07:59 -0300

    :test_tube: add test_get_users_raise_500_generic_error

--- tests/controllers/test_admin.py
+++ tests/controllers/test_admin.py
@@ -3,7 +3,7 @@ import pytest
 from httpx import AsyncClient
 
 from blog_api.commands.app import app
-from blog_api.contrib.errors import DatabaseError
+from blog_api.contrib.errors import DatabaseError, GenericError
 from blog_api.core.token import gen_jwt
 from blog_api.dependencies.auth import get_current_user
 from blog_api.repositories.users import UsersRepository
@@ -68,6 +68,38 @@ async def test_get_users_raise_500_database_error(
 
         assert result.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
         assert result.json() == {"detail": "Database integrity error"}
+
+        user_mock.assert_awaited_once()
+
+    app.dependency_overrides.clear()
+
+
+@pytest.mark.asyncio
+async def test_get_users_raise_500_generic_error(
+    mock_user,
+    client: AsyncClient,
+    admin_url,
+    mock_users_out_inserted,
+    mock_user_out_inserted,
+    user_agent,
+):
+    mock_user.role = "admin"
+    mock_user_out_inserted.role = "admin"
+
+    jwt = gen_jwt(360, mock_user)
+
+    app.dependency_overrides[get_current_user] = lambda: mock_user_out_inserted
+
+    with patch.object(
+        UsersRepository, "get_users", AsyncMock(side_effect=GenericError)
+    ) as user_mock:
+        result = await client.get(
+            f"{admin_url}/users",
+            headers={"Authorization": f"Bearer {jwt}", "User-Agent": user_agent},
+        )
+
+        assert result.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert result.json() == {"detail": "Generic Error"}
 
         user_mock.assert_awaited_once()
 
