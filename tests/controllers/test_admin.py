73cc39e366cf086596f377497d240dd5c16e4dce
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-27 16:48:57 -0300

    :white_check_mark: add test_get_users_return_success_from_cache

--- tests/controllers/test_admin.py
+++ tests/controllers/test_admin.py
@@ -51,6 +51,38 @@ async def test_get_users_return_success(
 
 
 @pytest.mark.asyncio
+async def test_get_users_return_success_from_cache(
+    mock_user,
+    client: AsyncClient,
+    admin_url,
+    mock_users_out_inserted,
+    mock_user_out_inserted,
+    user_agent,
+):
+    mock_user.role = "admin"
+    mock_user_out_inserted.role = "admin"
+
+    jwt = gen_jwt(360, mock_user)
+
+    app.dependency_overrides[get_current_user] = lambda: mock_user_out_inserted
+
+    with patch.multiple(
+        Cache,
+        get=AsyncMock(return_value=mock_users_out_inserted),
+        add=AsyncMock(return_value=None),
+    ):
+        result = await client.get(
+            f"{admin_url}/users",
+            headers={"Authorization": f"Bearer {jwt}", "User-Agent": user_agent},
+        )
+
+        assert result.status_code == status.HTTP_200_OK
+        assert len(result.json()) > 0
+
+    app.dependency_overrides.clear()
+
+
+@pytest.mark.asyncio
 async def test_get_users_raise_500_database_error(
     mock_user,
     client: AsyncClient,
