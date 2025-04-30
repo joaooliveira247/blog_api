8450d6169d4cced8398838d35a54935ad58e3359
joaooliveira247 <joaooliveira247@gmail.com>
2025-04-30 17:52:14 -0300

    :test_tube: add test_get_user_by_id_raise_500_cache_error_when_add_cache

--- tests/controllers/test_admin.py
+++ tests/controllers/test_admin.py
@@ -684,3 +684,43 @@ async def test_get_user_by_id_raise_500_generic_error(
         user_mock.assert_awaited_once()
 
     app.dependency_overrides.clear()
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_id_raise_500_cache_error_when_add_cache(
+    mock_user,
+    client: AsyncClient,
+    admin_url,
+    mock_user_out_inserted,
+    user_agent,
+):
+    mock_user.role = "admin"
+    mock_user_out_inserted.role = "admin"
+
+    jwt = gen_jwt(360, mock_user)
+
+    app.dependency_overrides[get_current_user] = lambda: mock_user_out_inserted
+
+    with (
+        patch.object(
+            UsersRepository,
+            "get_user_by_id",
+            AsyncMock(return_value=mock_user_out_inserted),
+        ) as user_mock,
+        patch.multiple(
+            Cache,
+            get=AsyncMock(return_value=None),
+            add=AsyncMock(side_effect=CacheError("Cache Error")),
+        ),
+    ):
+        result = await client.get(
+            f"{admin_url}/users/{mock_user_out_inserted.id}",
+            headers={"Authorization": f"Bearer {jwt}", "User-Agent": user_agent},
+        )
+
+        assert result.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert result.json() == {"detail": "Cache Error"}
+
+        user_mock.assert_awaited_once()
+
+    app.dependency_overrides.clear()
