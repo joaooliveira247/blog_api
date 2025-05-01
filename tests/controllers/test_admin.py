1e2e6b634409207deed4da013d9f465f328cc62f
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-01 09:27:28 -0300

    :test_tube: add test_update_user_role_raise_500_generic_error

--- tests/controllers/test_admin.py
+++ tests/controllers/test_admin.py
@@ -1084,4 +1084,34 @@ async def test_update_user_role_raise_500_unable_update_entity(
 
         mock_user.assert_awaited_once()
 
+
+@pytest.mark.asyncio
+async def test_update_user_role_raise_500_generic_error(
+    client: AsyncClient,
+    mock_user,
+    admin_url,
+    user_agent,
+    mock_user_out_inserted,
+):
+    mock_user.role = "admin"
+    mock_user_out_inserted.role = "admin"
+
+    jwt = gen_jwt(360, mock_user)
+
+    app.dependency_overrides[get_current_user] = lambda: mock_user_out_inserted
+
+    with patch.object(
+        UsersRepository, "update_user_role", AsyncMock(side_effect=GenericError)
+    ) as mock_user:
+        result = await client.patch(
+            f"{admin_url}/users/cb972123-113c-435d-9236-93d842489682/role",
+            headers={"Authorization": f"Bearer {jwt}", "User-Agent": user_agent},
+            json={"role": "dev"},
+        )
+
+        assert result.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert result.json() == {"detail": "Generic Error"}
+
+        mock_user.assert_awaited_once()
+
     app.dependency_overrides.clear()
