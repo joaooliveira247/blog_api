98ef8504814c7b7c13d753322d8d66be633bfd34
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-03 18:05:39 -0300

    :sparkles: add posts_controller in urls.py

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from blog_api.controllers.users import users_controller
 from blog_api.controllers.admin import admin_controller
+from blog_api.controllers.posts import posts_controller
 
 api_router = APIRouter()
 api_router.include_router(users_controller, prefix="/account")
 api_router.include_router(admin_controller, prefix="/admin")
+api_router.include_router(posts_controller, prefix="/posts")
