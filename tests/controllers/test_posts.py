f03587abb7526257b4668288cb8b57cac03721eb
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-12 06:52:10 -0300

    :test_tube: add test_get_posts_by_user_id_raise_500_cache_error_from_cache

--- tests/controllers/test_posts.py
+++ tests/controllers/test_posts.py
@@ -768,3 +768,29 @@ async def test_get_posts_by_user_id_success_from_cache(
 
         assert result.status_code == status.HTTP_200_OK
         assert len(result.json()["items"]) > 1
+
+
+@pytest.mark.asyncio
+async def test_get_posts_by_user_id_raise_500_cache_error_from_cache(
+    client: AsyncClient,
+    posts_url: str,
+    user_agent: str,
+    user_id: UUID,
+    mock_posts_inserted: list[PostOut],
+):
+    for post in mock_posts_inserted:
+        post.author == "Username"
+
+    with patch.object(
+        Cache,
+        "get",
+        AsyncMock(side_effect=CacheError("Cache Error")),
+    ) as mock_post:
+        result = await client.get(
+            f"{posts_url}/user/{user_id}", headers={"User-Agent": user_agent}
+        )
+
+        mock_post.assert_awaited_once()
+
+        assert result.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert result.json() == {"detail": "Cache Error"}
