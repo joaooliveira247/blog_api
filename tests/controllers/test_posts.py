8b33174f24b0beb6967d6a956aa9a07a0b32812f
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-12 06:41:41 -0300

    :test_tube: add test_get_posts_by_user_id_raise_500_database_error

--- tests/controllers/test_posts.py
+++ tests/controllers/test_posts.py
@@ -676,3 +676,36 @@ async def test_get_posts_by_user_id_return_success(
 
         assert result.status_code == status.HTTP_200_OK
         assert len(result.json()["items"]) > 1
+
+
+@pytest.mark.asyncio
+async def test_get_posts_by_user_id_raise_500_database_error(
+    client: AsyncClient,
+    posts_url: str,
+    user_agent: str,
+    user_id: UUID,
+    mock_posts_inserted: list[PostOut],
+):
+    for post in mock_posts_inserted:
+        post.author == "Username"
+
+    with (
+        patch.object(
+            PostsRepository,
+            "get_posts_by_user_id",
+            AsyncMock(side_effect=DatabaseError),
+        ) as mock_post,
+        patch.multiple(
+            Cache,
+            get=AsyncMock(return_value=None),
+            add=AsyncMock(return_value=None),
+        ),
+    ):
+        result = await client.get(
+            f"{posts_url}/user/{user_id}", headers={"User-Agent": user_agent}
+        )
+
+        mock_post.assert_awaited_once()
+
+        assert result.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert result.json() == {"detail": "Database integrity error"}
