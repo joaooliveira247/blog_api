878b2562c13fae62b7da91ca070da4985ff88723
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-14 09:20:05 -0300

    :sparkles: add update_post with route PUT /posts/{post_id}

--- blog_api/controllers/posts.py
+++ blog_api/controllers/posts.py
@@ -8,13 +8,14 @@ from blog_api.contrib.errors import (
     EncodingError,
     GenericError,
     UnableCreateEntity,
+    UnableUpdateEntity,
 )
 from blog_api.core.cache import Cache
 from blog_api.dependencies.auth import get_current_user
 from blog_api.dependencies.dependencies import CacheDependency, DatabaseDependency
 from blog_api.models.posts import PostModel
 from blog_api.repositories.posts import PostsRepository
-from blog_api.schemas.posts import PostIn, PostOut
+from blog_api.schemas.posts import PostIn, PostOut, PostUpdate
 from blog_api.schemas.response import PostCreatedSchema
 from blog_api.schemas.users import UserOut
 
@@ -145,6 +146,41 @@ async def get_post_by_user_id(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e.message
         )
     except (CacheError, EncodingError) as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e.message
+        )
+    except GenericError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e.message
+        )
+
+
+@posts_controller.put("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_post(
+    db: DatabaseDependency,  # type: ignore
+    post_id: UUID,
+    user: UserOut = Depends(get_current_user),
+    body: PostUpdate = Body(...),
+) -> None:
+    repository = PostsRepository(db)
+
+    try:
+        post = await repository.get_post_by_id(post_id)
+
+        if not post:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
+            )
+
+        if post.author_id is not user.id:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail=f"{post_id} not belongs current user",
+            )
+
+        await repository.update_post(post_id, body.model_dump())
+
+    except (DatabaseError, UnableUpdateEntity) as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e.message
         )
