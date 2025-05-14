414c1a90d83c7e63722123423eb5be9c05ae7152
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-14 09:10:19 -0300

    :test_tube: add test_update_post_raise_404_post_not_found

--- tests/controllers/test_posts.py
+++ tests/controllers/test_posts.py
@@ -989,3 +989,38 @@ async def test_update_post_raise_422_invalid_body(
     assert result.json()["detail"][0]["msg"] == "Input should be a valid string"
 
     app.dependency_overrides.clear()
+
+
+@pytest.mark.asyncio
+async def test_update_post_raise_404_post_not_found(
+    client: AsyncClient,
+    posts_url: str,
+    user_agent: str,
+    mock_user_out_inserted: UserOut,
+    mock_user,
+    mock_post_inserted,  # noqa: F811
+    mock_update_post,
+):
+    mock_post_inserted.author_id = mock_user_out_inserted.id
+
+    jwt = gen_jwt(360, mock_user)
+
+    app.dependency_overrides[get_current_user] = lambda: mock_user_out_inserted
+
+    with patch.object(
+        PostsRepository,
+        "get_post_by_id",
+        AsyncMock(return_value=None),
+    ) as mock_post:
+        result = await client.put(
+            f"{posts_url}/{mock_post_inserted.id}",
+            headers={"Authorization": f"Bearer {jwt}", "User-Agent": user_agent},
+            json=mock_update_post,
+        )
+
+        mock_post.assert_awaited_once_with(mock_post_inserted.id)
+
+        assert result.status_code == status.HTTP_404_NOT_FOUND
+        assert result.json() == {"detail": "Post not found"}
+
+    app.dependency_overrides.clear()
