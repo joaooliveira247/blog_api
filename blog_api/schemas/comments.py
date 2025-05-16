95251f374ee0a2d5196f954ad61e05530dac0e03
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-16 18:54:04 -0300

    :sparkles: add post_id, author_username, author_id fields in CommentOut schema

--- blog_api/schemas/comments.py
+++ blog_api/schemas/comments.py
@@ -1,5 +1,7 @@
 from uuid import UUID
+
 from pydantic import BaseModel, Field
+
 from blog_api.contrib.schemas import OutMixin
 
 
@@ -8,8 +10,10 @@ class CommentBase(BaseModel):
 
 
 class CommentOut(CommentBase, OutMixin):
+    post_id: UUID = Field(..., description="Post id")
     post_title: str = Field(..., description="Title post")
-    author: str = Field(..., description="Comment author username")
+    author_id: UUID = Field(..., description="Comment author id")
+    author_username: str = Field(..., description="Comment author username")
 
 
 class CommentIn(CommentBase):
