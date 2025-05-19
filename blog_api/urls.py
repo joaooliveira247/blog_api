61828e713e120c3d5bd80b773a97a0edc0f034ea
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-19 11:21:33 -0300

    :sparkles: add comments_controller in urls.py with prefix /comments

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
-from blog_api.controllers.users import users_controller
+
 from blog_api.controllers.admin import admin_controller
+from blog_api.controllers.comments import comments_controller
 from blog_api.controllers.posts import posts_controller
+from blog_api.controllers.users import users_controller
 
 api_router = APIRouter()
 api_router.include_router(users_controller, prefix="/account")
 api_router.include_router(admin_controller, prefix="/admin")
 api_router.include_router(posts_controller, prefix="/posts")
+api_router.include_router(comments_controller, prefix="/comments")
