8ea34ee1010d135c68503efdd5bd139f9a0b6312
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-24 07:23:23 -0300

    :test_tube: add test_get_comments_by_user_id_raise_500_database_error

--- tests/controllers/test_comments.py
+++ tests/controllers/test_comments.py
@@ -512,3 +512,30 @@ async def test_get_comments_by_user_id_raise_404_no_result_found(
         assert result.json() == {"detail": "Result not found with user_id"}
 
         mock_comments.assert_awaited_once()
+
+
+@pytest.mark.asyncio
+async def test_get_comments_by_user_id_raise_500_database_error(
+    client: AsyncClient, comments_url, user_agent, post_id
+):
+    with (
+        patch.object(
+            CommentsRepository,
+            "get_comments_by_user_id",
+            AsyncMock(side_effect=DatabaseError),
+        ) as mock_comments,
+        patch.multiple(
+            Cache,
+            get=AsyncMock(return_value=None),
+            add=AsyncMock(return_value=None),
+        ),
+    ):
+        result = await client.get(
+            f"{comments_url}/user/{post_id}",
+            headers={"User-Agent": user_agent},
+        )
+
+        assert result.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert result.json() == {"detail": "Database integrity error"}
+
+        mock_comments.assert_awaited_once()
