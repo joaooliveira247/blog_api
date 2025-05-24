01676d622588bd6eedeb1fa7b27e9900aa3e8fd9
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-24 09:28:03 -0300

    :test_tube: add test_get_comments_by_user_id_raise_500_cache_error_when_add

--- tests/controllers/test_comments.py
+++ tests/controllers/test_comments.py
@@ -606,3 +606,34 @@ async def test_get_comments_by_user_id_raise_500_generic_error_from_cache(
         assert result.json() == {"detail": "Generic Error"}
 
         mock_comments.assert_awaited_once()
+
+
+@pytest.mark.asyncio
+async def test_get_comments_by_user_id_raise_500_cache_error_when_add(
+    client: AsyncClient,
+    comments_url,
+    user_agent,
+    user_id,
+    mock_comments_inserted_same_author,
+):
+    with (
+        patch.object(
+            CommentsRepository,
+            "get_comments_by_user_id",
+            AsyncMock(return_value=mock_comments_inserted_same_author),
+        ) as mock_comments,
+        patch.multiple(
+            Cache,
+            get=AsyncMock(return_value=None),
+            add=AsyncMock(side_effect=CacheError("Cache Error")),
+        ),
+    ):
+        result = await client.get(
+            f"{comments_url}/user/{user_id}",
+            headers={"User-Agent": user_agent},
+        )
+
+        assert result.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert result.json() == {"detail": "Cache Error"}
+
+        mock_comments.assert_awaited_once()
