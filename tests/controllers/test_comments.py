7f9ee33373f9c0c95d3921d50f6a5e382ac30a29
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-25 18:58:32 -0300

    :test_tube: add test_update_comment_raise_500_generic_error

--- tests/controllers/test_comments.py
+++ tests/controllers/test_comments.py
@@ -908,3 +908,38 @@ async def test_update_comment_raise_500_unable_update_entity(
         assert result.json() == {"detail": "Unable Update Entity"}
 
     app.dependency_overrides.clear()
+
+
+@pytest.mark.asyncio
+async def test_update_comment_raise_500_generic_error(
+    client: AsyncClient,
+    comments_url: str,
+    user_agent: str,
+    mock_user,
+    mock_comment_inserted,
+    mock_user_out_inserted,
+):
+    mock_user.id = mock_comment_inserted.author_id
+    mock_user_out_inserted.id = mock_comment_inserted.author_id
+
+    jwt = gen_jwt(360, mock_user)
+    app.dependency_overrides[get_current_user] = lambda: mock_user_out_inserted
+
+    with patch.multiple(
+        CommentsRepository,
+        get_comment_by_id=AsyncMock(return_value=mock_comment_inserted),
+        update_comment=AsyncMock(side_effect=GenericError),
+    ):
+        result = await client.put(
+            f"{comments_url}/{mock_comment_inserted.id}",
+            headers={
+                "Authorization": f"Bearer {jwt}",
+                "User-Agent": user_agent,
+            },
+            json={"content": "update my comment"},
+        )
+
+        assert result.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert result.json() == {"detail": "Generic Error"}
+
+    app.dependency_overrides.clear()
