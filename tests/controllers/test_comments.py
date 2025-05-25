b68c76f6a91c6ad912e915181eceeab1aa7418a3
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-25 18:48:34 -0300

    :test_tube: add test_update_comment_raise_401_comment_dont_belongs_current_user

--- tests/controllers/test_comments.py
+++ tests/controllers/test_comments.py
@@ -768,3 +768,37 @@ async def test_update_comment_raise_404_not_found(
         assert result.json() == {"detail": "Comment not found."}
 
     app.dependency_overrides.clear()
+
+
+@pytest.mark.asyncio
+async def test_update_comment_raise_401_comment_dont_belongs_current_user(
+    client: AsyncClient,
+    comments_url: str,
+    user_agent: str,
+    mock_user,
+    mock_comment_inserted,
+    mock_user_out_inserted,
+):
+    jwt = gen_jwt(360, mock_user)
+    app.dependency_overrides[get_current_user] = lambda: mock_user_out_inserted
+
+    with patch.object(
+        CommentsRepository,
+        "get_comment_by_id",
+        AsyncMock(return_value=mock_comment_inserted),
+    ):
+        result = await client.put(
+            f"{comments_url}/{mock_comment_inserted.id}",
+            headers={
+                "Authorization": f"Bearer {jwt}",
+                "User-Agent": user_agent,
+            },
+            json={"content": "update my comment"},
+        )
+
+        assert result.status_code == status.HTTP_401_UNAUTHORIZED
+        assert result.json() == {
+            "detail": "This comment don't belongs current user"
+        }
+
+    app.dependency_overrides.clear()
