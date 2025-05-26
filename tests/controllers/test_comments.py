75ae85c2493f8a94d86d7afdac3e46b9a1b2632b
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-26 08:31:24 -0300

    :test_tube: add test_delete_comment_raise_404_comment_not_found

--- tests/controllers/test_comments.py
+++ tests/controllers/test_comments.py
@@ -977,3 +977,39 @@ async def test_delete_comment_success(
         assert result.text == ""
 
     app.dependency_overrides.clear()
+
+
+@pytest.mark.asyncio
+async def test_delete_comment_raise_404_comment_not_found(
+    client: AsyncClient,
+    comments_url: str,
+    user_agent: str,
+    mock_user,
+    mock_comment_inserted,
+    mock_user_out_inserted,
+):
+    mock_user.id = mock_comment_inserted.author_id
+    mock_user_out_inserted.id = mock_comment_inserted.author_id
+
+    jwt = gen_jwt(360, mock_user)
+    app.dependency_overrides[get_current_user] = lambda: mock_user_out_inserted
+
+    with patch.object(
+        CommentsRepository,
+        "get_comment_by_id",
+        AsyncMock(return_value=None),
+    ) as mock_comment:
+        result = await client.delete(
+            f"{comments_url}/{mock_comment_inserted.id}",
+            headers={
+                "Authorization": f"Bearer {jwt}",
+                "User-Agent": user_agent,
+            },
+        )
+
+        mock_comment.assert_awaited_once()
+
+        assert result.status_code == status.HTTP_404_NOT_FOUND
+        assert result.json() == {"detail": "Comment not found."}
+
+    app.dependency_overrides.clear()
