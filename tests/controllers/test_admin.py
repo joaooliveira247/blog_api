54b34db91c4a477ab26cf7d3aa7873caab9b2016
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-28 10:46:58 -0300

    :test_tube: add test_delete_comment_raise_404_not_found

--- tests/controllers/test_admin.py
+++ tests/controllers/test_admin.py
@@ -1968,3 +1968,42 @@ async def test_delete_comment_raise_401_unauthorized_as_dev(
     assert result.json() == {"detail": "invalid permissions"}
 
     app.dependency_overrides.clear()
+
+
+@pytest.mark.asyncio
+async def test_delete_comment_raise_404_not_found(
+    client: AsyncClient,
+    admin_url: str,
+    comments_url: str,
+    user_agent: str,
+    mock_user,
+    mock_comment_inserted,
+    mock_user_out_inserted,
+):
+    mock_user.id = mock_comment_inserted.author_id
+    mock_user.role = "admin"
+    mock_user_out_inserted.id = mock_comment_inserted.author_id
+    mock_user_out_inserted.role = "admin"
+
+    jwt = gen_jwt(360, mock_user)
+    app.dependency_overrides[get_current_user] = lambda: mock_user_out_inserted
+
+    with patch.object(
+        CommentsRepository,
+        "get_comment_by_id",
+        AsyncMock(return_value=None),
+    ) as mock_comment:
+        result = await client.delete(
+            f"{admin_url}{comments_url}/{mock_comment_inserted.id}",
+            headers={
+                "Authorization": f"Bearer {jwt}",
+                "User-Agent": user_agent,
+            },
+        )
+
+        assert result.status_code == status.HTTP_404_NOT_FOUND
+        assert result.json() == {"detail": "Comment not found."}
+
+        mock_comment.assert_awaited_once()
+
+    app.dependency_overrides.clear()
