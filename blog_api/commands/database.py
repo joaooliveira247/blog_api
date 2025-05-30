cc049202e6bbb8c7ab9daaa222b3670674b64def
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-30 18:20:18 -0300

    :sparkles: add database_init_lifespan fastapi lifespan to create all tables in database

--- blog_api/commands/database.py
+++ blog_api/commands/database.py
@@ -1,5 +1,11 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from fastapi import FastAPI
 from sqlalchemy import inspect
 
+from blog_api.contrib.models import BaseModel
+from blog_api.core.database import engine
 from blog_api.models import (  # noqa: F401  # pylint: disable=unused-import
     comments,
     posts,
@@ -10,3 +16,20 @@ from blog_api.models import (  # noqa: F401  # pylint: disable=unused-import
 def get_table_names(sync_conn):
     inspector = inspect(sync_conn)
     return inspector.get_table_names()
+
+
+@asynccontextmanager
+async def database_init_lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    async with engine.begin() as conn:
+        existing_tables = await conn.run_sync(get_table_names)
+        all_tables = BaseModel.metadata.tables.keys()
+
+        for table in all_tables:
+            if table not in existing_tables:
+                print(f"✅ {table} created")
+
+        await conn.run_sync(
+            lambda sync_conn: BaseModel.metadata.create_all(bind=sync_conn)
+        )
+
+    yield
