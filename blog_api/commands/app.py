571623e2dee40243e82ac82f90d5c74ecfcb384a
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-30 18:21:01 -0300

    :sparkles: add lifespan arg in fastapi class

--- blog_api/commands/app.py
+++ blog_api/commands/app.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
+
+from blog_api.commands.database import database_init_lifespan
 from blog_api.core.config import get_settings
-from blog_api.urls import api_router
 from blog_api.middlewares.user_agent import UserAgentMiddleware
+from blog_api.urls import api_router
 
 settings = get_settings()
 
 app = FastAPI(
-    title=settings.PROJECT_NAME, docs_url=None, redoc_url=None, openapi_url=None
+    title=settings.PROJECT_NAME,
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None,
+    lifespan=database_init_lifespan,
 )
 app.add_middleware(UserAgentMiddleware)
 app.include_router(api_router)
