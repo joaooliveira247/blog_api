77af2b9f985f7d4f929f84a48683f9c465e37190
joaooliveira247 <joaooliveira247@gmail.com>
2025-05-31 16:47:41 -0300

    :sparkles: add update_role cli command to change user role

--- blog_api/commands/cmd.py
+++ blog_api/commands/cmd.py
@@ -1,6 +1,10 @@
+import asyncio
 from enum import Enum
+from uuid import UUID
 
-from typer import Typer
+from typer import Exit, Typer, echo
+
+from blog_api.commands.database import cli_update_user_role
 
 app = Typer()
 
@@ -9,3 +13,17 @@ class Role(str, Enum):
     admin = "admin"
     dev = "dev"
     user = "user"
+
+
+@app.command()
+def update_role(user_id: UUID, role: Role):
+    """
+    Change user Role.
+    """
+    try:
+        echo(f"⚠️ Changing user's role {user_id} to {role}")
+        asyncio.run(cli_update_user_role(user_id, role))
+        echo("✅ Role changed successfully!")
+    except Exception as e:
+        echo(f"Error: {e}")
+        raise Exit(code=1)
